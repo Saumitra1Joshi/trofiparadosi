f9215407b0cd93f3adba4798a80ea2f5c3cf39d5
Umang Singhal <Umang.Singhal@BRILLIO.COM>
2022-08-12 12:27:37 +0530

    second commit

---
+++ backend/src/main/java/com/fooddelivery/deliveryserver/DeliveryserverApplication.java
@@ -0,0 +1,13 @@
+package com.fooddelivery.deliveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryserverApplication.class, args);
+	}
+
+}
