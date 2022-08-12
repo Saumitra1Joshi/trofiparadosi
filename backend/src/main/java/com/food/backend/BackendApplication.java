94f57d2ea403d09d4f0be9e116ad7a5723fd7edb
Umang Singhal <Umang.Singhal@BRILLIO.COM>
2022-08-12 12:52:29 +0530

    fourth commit

--- backend/src/main/java/com/food/backend/BackendApplication.java
+++ backend/src/main/java/com/food/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.fooddelivery.deliveryserver;
+package com.food.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeliveryserverApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeliveryserverApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/food/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/food/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.fooddelivery.deliveryserver;
+package com.food.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DeliveryserverApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
